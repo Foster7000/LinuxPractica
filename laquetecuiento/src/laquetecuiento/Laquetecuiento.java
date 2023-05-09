be1ea9fbe5f4461cee551621422f905d56fe17ba
Foster7000 <dani70granada@gmail.com>
2023-05-09 19:21:57 +0200

    Hola

--- laquetecuiento/src/laquetecuiento/Laquetecuiento.java
+++ laquetecuiento/src/laquetecuiento/Laquetecuiento.java
@@ -7,6 +7,7 @@ public class Laquetecuiento {
  
     public static void main(String[] args) {
         System.out.println("Buenas que tal");
+        System.out.println("Que buen tiempo hace");
     }
     
 }
